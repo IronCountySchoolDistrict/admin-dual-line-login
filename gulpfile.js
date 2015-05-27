73d2176f2b080a98f66d73e6a3b0f6c7536c65b0
Nathan Jones <purkinje90@gmail.com>
2015-05-27 16:29:41 -0600

    Plugins should be defined in the gulp-ps-tasks tasks file so every plugin folder doesn't need to install its own gulp plugins. Use gulp-ps-tasks's gulp plugins instead.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,3 @@
 var gulp = require("gulp");
-var plugins = require("gulp-load-plugins")();
-plugins.del = require("del");
 
-
-require("../gulp-ps-tasks/tasks")(gulp, plugins);
+require("../gulp-ps-tasks/tasks")(gulp);
